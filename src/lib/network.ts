9614c23acc44773748394ebf7eca0a8c41ff0a4a
Enzo prados <enzo.prados@gmail.com>
2025-11-06 09:08:02 +0000

    donc la ta fait : corporate-proxy
    fait aussi un pc link a rien pour Hyp

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -3,6 +3,7 @@ import playerPc from './network/pcs/player-pc.json';
 import holbertonGateway from './network/pcs/holberton-gateway.json';
 import workstation01 from './network/pcs/workstation-01.json';
 import corporateProxy from './network/pcs/corporate-proxy.json';
+import hypnetSearch from './network/pcs/hypnet-search.json';
 import { type PC } from './network/types';
 
 export const network: PC[] = [
@@ -10,6 +11,5 @@ export const network: PC[] = [
     holbertonGateway,
     workstation01,
     corporateProxy,
+    hypnetSearch,
 ];
-
-    
