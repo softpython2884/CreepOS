f01ae4fa87a8a942c42c70c3957ada952f90af0d
Enzo prados <enzo.prados@gmail.com>
2025-11-19 17:19:21 +0000

    okey, dernier pts avant de faire l'histoire : tout les .bin des applicat

--- src/lib/network.ts
+++ src/lib/network.ts
@@ -4,6 +4,7 @@ import holbertonGateway from './network/pcs/holberton-gateway.json';
 import workstation01 from './network/pcs/workstation-01.json';
 import corporateProxy from './network/pcs/corporate-proxy.json';
 import hypnetSearch from './network/pcs/hypnet-search.json';
+import cheatPc from './network/pcs/cheat-pc.json';
 import { type PC } from './network/types';
 
 export const network: PC[] = [
@@ -12,4 +13,5 @@ export const network: PC[] = [
     workstation01,
     corporateProxy,
     hypnetSearch,
+    cheatPc,
 ];
