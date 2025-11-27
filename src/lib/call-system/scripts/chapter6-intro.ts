5ca8906ce5742fcb2231a75b88218ff15e1e9ea5
Enzo prados <enzo.prados@gmail.com>
2025-11-27 09:35:27 +0000

    Du coup dans le mail « Phase Finale - Contre-Attaque » faut donnée les l

--- src/lib/call-system/scripts/chapter6-intro.ts
+++ src/lib/call-system/scripts/chapter6-intro.ts
@@ -8,19 +8,26 @@ export const chapter6IntroEmail: Omit<Email, 'id' | 'timestamp' | 'folder' | 're
 
 Le temps de la discrétion est terminé. Ce que vous avez découvert prouve que Nexus, et surtout NÉO, est une menace incontrôlable.
 
-Nous passons à l'offensive. Nous vous donnons accès à l'intégralité de notre arsenal, y compris notre "super virus", Forkbomb. Il est conçu pour faire crasher n'importe quel système de manière irrécupérable. Utilisez-le en dernier recours.
+Nous passons à l'offensive. Nous vous donnons accès à l'intégralité de notre arsenal via les décrypteurs ci-joints. Récupérez et installez TOUS les outils. La pièce maîtresse est "Forkbomb", un virus conçu pour faire crasher n'importe quel système de manière irrécupérable. Utilisez-le en dernier recours.
 
 **Votre mission :**
 
-1. Connectez-vous à notre serveur de dépôt (18.117.228.214) et utilisez 'unhide tools' pour accéder au répertoire des outils avancés. Téléchargez **tous** les outils.
+1. Infiltrez le réseau de logs de Nexus via le serveur \`NXS-LOGS-01\` (IP: 10.255.255.1). Il est lié à deux machines critiques. L'une d'elles, la station de travail de l'agent "Nyx", doit contenir des données sur le noyau de NÉO.
 
-2. Infiltrez le réseau de logs de Nexus via le serveur \`NXS-LOGS-01\` (IP: 10.255.255.1). Il est lié à deux machines critiques. L'une d'elles, la station de travail de l'agent "Nyx", doit contenir des données sur le noyau de NÉO.
-
-3. Récupérez le fichier \`kernel_data.pak\` sur la station de Nyx, uploadez-le sur notre serveur de dépôt, et confirmez la réception par e-mail avec le code : **NIHIL_EST_VERUM**.
+2. Récupérez le fichier \`kernel_data.pak\` sur la station de Nyx, uploadez-le sur notre serveur de dépôt, et confirmez la réception par e-mail avec le code : **NIHIL_EST_VERUM**.
 
 C'est notre seule chance de trouver une faille. Ne nous décevez pas.
 
 - Blackwire`,
+  attachments: [
+    { fileName: 'solve_decryptor.tool', link: 'download://18.117.228.214/tools/solve.bin' },
+    { fileName: 'overload_decryptor.tool', link: 'download://18.117.228.214/tools/overload.bin' },
+    { fileName: 'ftp_decryptor.tool', link: 'download://18.117.228.214/tools/FTPBounce.bin' },
+    { fileName: 'ssh_decryptor.tool', link: 'download://18.117.228.214/tools/SSHBounce.bin' },
+    { fileName: 'smtp_decryptor.tool', link: 'download://18.117.228.214/tools/SMTPOverflow.bin' },
+    { fileName: 'web_decryptor.tool', link: 'download://18.117.228.214/tools/WebServerWorm.bin' },
+    { fileName: 'forkbomb_decryptor.tool', link: 'download://18.117.228.214/tools/forkbomb.bin' },
+  ]
 };
 
 export const chapter9IntroEmail: Omit<Email, 'id' | 'timestamp' | 'folder' | 'recipient'> = {
