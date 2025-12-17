abbb10d42c811b2cd2b591e37857722e0ff43837
Enzo prados <enzo.prados@gmail.com>
2025-12-17 12:44:00 +0000

    code change

--- src/lib/call-system/scripts/chapter5-intro.ts
+++ src/lib/call-system/scripts/chapter5-intro.ts
@@ -11,11 +11,11 @@ Nous vous avons fourni un nouvel outil, FTPBounce.bin, qui devrait maintenant ê
 
 Votre nouvelle mission est d'infiltrer une station de travail d'un superviseur de Nexus. Cependant, l'accès direct est impossible. Vous devrez d'abord passer par un nœud de service moins sécurisé.
 
-1.  Infiltrez le serveur 'Service-Node-4B' à l'adresse **172.16.4.2**. Il est peu protégé. Cherchez des indices.
+1.  Infiltrez le serveur 'Service-Node-4B' à l'adresse 172.16.4.2. Il est peu protégé. Cherchez des indices.
 2.  Utilisez les informations trouvées pour localiser et attaquer la vraie cible. Attendez-vous à de lourdes défenses (pare-feu, proxy, plusieurs ports à ouvrir).
 3.  Une fois à l'intérieur, copiez tous les rapports que vous trouverez.
 4.  Uploadez ces rapports sur notre serveur de dépôt (18.117.228.214) dans le répertoire /upload.
-5.  Envoyez-nous un e-mail de confirmation avec le code **CODE_NEO_V4**.
+5.  Envoyez-nous un e-mail de confirmation avec le code 4B-NdResult.
 
 Faites vite. Nous n'avons aucune idée de ce que NÉO est en train de devenir.
 
